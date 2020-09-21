04f2c5b0dd38dcef9e068ce6e9a9823a7f218eba
MerithzDTU <s195480@student.dtu.dk>
2020-09-21 18:31:37 +0200

    Alt der er lavet indtil videre

---
+++ app/src/main/java/com/example/galgeleg/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.galgeleg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button spilKnap, hjaelpKnap, highscoreKnap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
