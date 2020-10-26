184647b8ac54d79afc5f034dbe8c4a5cb0ee7931
MerithzDTU <s195480@student.dtu.dk>
2020-10-26 22:38:34 +0100

    Flere ting tilføjet: Tilføjelser til inputfeltet > Kun bogstaver fra dansk alfabet > Resized og ændret hintfeltet Ændringer til spilinfo: > Ændret spilinfoen på siden, da der stod en tand for meget. (KeepItSimple).
    Galge billeder
    > tilføjet drawables af galge + "hanging man"
    > switch case, der opdaterer billedet efter antal forkerte gæt på bogstaver.
    
    Nye metoder
    > initialize() - opsætter forbindelsen mellem java-aktiviteten og xml-layout
    > genstart() - genstarter spillet efter spillets afslutning.
    > gætBogstav() - inputhandling og errorhandling for gættefeltet.
    > skjulTastatur() - skjuler tastaturet når spillet er afsluttet.
    
    (SORRY FOR LANGT COMMIT, JEG VAR WIRED IN ;-) )

--- app/src/main/java/com/example/galgeleg/Spillet_akt.java
+++ app/src/main/java/com/example/galgeleg/Spillet_akt.java
@@ -3,86 +3,159 @@ package com.example.galgeleg;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.InputFilter;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TextView;
 
-public class Spillet_akt extends AppCompatActivity implements View.OnClickListener {
+public class Spillet_akt extends AppCompatActivity {
     Galgelogik logik = new Galgelogik();
     Animation animation;
     Button sendSvar;
     TextView spilInfo;
+    TextView ordet;
     EditText gaetteFelt;
+    ImageView imageview;
+    Button genstart;
     int maxBogstav = 1;
+
+    private void initialize() {
+        animation = AnimationUtils.loadAnimation(this, R.anim.ryst);
+        spilInfo = findViewById(R.id.spilInfo);
+        imageview = findViewById(R.id.imageView);
+        gaetteFelt = findViewById(R.id.gaetteFelt);
+        sendSvar = findViewById(R.id.sendSvar);
+        ordet = findViewById(R.id.ordet);
+        genstart = findViewById(R.id.genstart);
+
+        genstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                genstart();
+            }
+        });
+
+        sendSvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gætBogstav();
+            }
+        });
+
+        gaetteFelt.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    gætBogstav();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        animation = AnimationUtils.loadAnimation(this, R.anim.ryst);
         setContentView(R.layout.activity_spillet_akt);
-        //Programmatisk layout
-
-        spilInfo = findViewById(R.id.spilInfo);
-        spilInfo.setText("Velkommen til et deprimerende spil, hvis du altså ikke gætter rigtigt!" +
-                "\nDu skal gætte dette ord: " + logik.getSynligtOrd() +
-                "\nSmid et bogstav ind i feltet nedenunder og tryk på knappen!\n");
-
-        String velkomst = getIntent().getStringExtra("velkomst");
-        if (velkomst != null) spilInfo.append(velkomst);
-
-
-        gaetteFelt = findViewById(R.id.gaetteFelt);
-        gaetteFelt.setHint("Det er her bogstavet skal gættes!");
-        gaetteFelt.setFilters(new InputFilter[] {new InputFilter.LengthFilter(maxBogstav)});
-
-        sendSvar = findViewById(R.id.sendSvar);
-        sendSvar.setText("Spil");
-
-        //sendSvar.setCompoundDrawablesRelativeWithIntrinsicBounds(android.R.drawable.ic_media_play,0,0,0);
-
-
-        logik.logStatus(); //rigtige i loggen
+        initialize();
+        genstart();
     }
 
-    @Override
-    public void onClick(View v) {
-        System.out.println("1");
-        String bogstav = gaetteFelt.getText().toString();
-        if(bogstav.length() != 1){
-            gaetteFelt.setError("KUN ÉT BOGSTAV!");
-            return;
-        }
-        System.out.println("2");
+    public void genstart(){
+        logik.nulstil();
+        spilInfo.setText("Gæt ordet gemt med stjerner!" + "\nIndtast et bogstav i feltet og prøv lykken!\n");
+        ordet.setText(logik.getSynligtOrd());
+        gaetteFelt.setFilters(new InputFilter[]{new InputFilter.LengthFilter(maxBogstav)});
+        logik.logStatus(); //rigtige i loggen
+
+        genstart.setVisibility(View.INVISIBLE);
+        sendSvar.setVisibility(View.VISIBLE);
+        gaetteFelt.setVisibility(View.VISIBLE);
+        imageview.setImageResource(R.drawable.galge);
+    }
+
+    public void gætBogstav(){
+        final String bogstav = gaetteFelt.getText().toString();
         logik.gætBogstav(bogstav);
-        System.out.println("3");
         gaetteFelt.setText("");
         gaetteFelt.setError(null);
         animation.reset();
-        if(!logik.getOrdet().contains(bogstav)){
+
+        opdaterSkærm(bogstav);
+    }
+
+    private void skjulTastatur(){
+        if(gaetteFelt != null){
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(gaetteFelt.getWindowToken(),0);
+        }
+    }
+
+    private void opdaterSkærm(String bogstav) {
+        ordet.setText(logik.getSynligtOrd());
+        spilInfo.setText("\n\nDu har " + logik.getAntalForkerteBogstaver() + " forkerte: " + logik.getForkerteBogstaver());
+
+        if (logik.erSpilletVundet()) {
+            spilInfo.clearComposingText();
+            spilInfo.append("\nDu har vundet med " + logik.getBrugteBogstaver().size() + " forsøg!");
+            gaetteFelt.setVisibility(View.INVISIBLE);
+            skjulTastatur();
+            sendSvar.setVisibility(View.INVISIBLE);
+            genstart.setVisibility(View.VISIBLE);
+
+        }
+        if(logik.erSpilletTabt()) {
+            spilInfo.append("\nDu har tabt! Ordet var: " + logik.getOrdet());
+            gaetteFelt.setVisibility(View.INVISIBLE);
+            gaetteFelt.clearFocus();
+            skjulTastatur();
+            sendSvar.setVisibility(View.INVISIBLE);
+            genstart.setVisibility(View.VISIBLE);
+        }
+
+        if (!logik.getOrdet().contains(bogstav)) {
             sendSvar.clearAnimation();
             sendSvar.startAnimation(animation);
+            switch (logik.getAntalForkerteBogstaver()){
+                case 0:
+                    imageview.setImageResource(R.drawable.galge);
+                    break;
+                case 1:
+                    imageview.setImageResource(R.drawable.forkert1);
+                    break;
+                case 2:
+                    imageview.setImageResource(R.drawable.forkert2);
+                    break;
+                case 3:
+                    imageview.setImageResource(R.drawable.forkert3);
+                    break;
+                case 4:
+                    imageview.setImageResource(R.drawable.forkert4);
+                    break;
+                case 5:
+                    imageview.setImageResource(R.drawable.forkert5);
+                    break;
+                case 6:
+                    imageview.setImageResource(R.drawable.forkert6);
+                    break;
+                default:
+                    imageview.setImageResource(R.drawable.forkert6);
+                    break;
+            }
         }
-        opdaterSkærm();
-        System.out.println("4");
     }
-    private void opdaterSkærm(){
-        spilInfo.setText("Gæt ordet: " + logik.getSynligtOrd());
-        spilInfo.append("\n\nDu har " + logik.getAntalForkerteBogstaver() + " forkerte: " + logik.getForkerteBogstaver());
-
-        if(logik.erSpilletVundet()){
-            spilInfo.append("\nDu har vundet!");
-        }
-        if(logik.erSpilletTabt()){
-            spilInfo.append("\nDu har tabt! Ordet var: " + logik.getOrdet());
-        }
-    }
-
 }
